55a3df42891a129de08dca50a4dd2633ca26b05a
NgTZ <warinpalm@gmail.com>
2024-11-09 21:38:35 +0700

    Update SingIn.jsx

--- src/Pages/Login/signin/SingIn.jsx
+++ src/Pages/Login/signin/SingIn.jsx
@@ -15,7 +15,7 @@ const SingIn = () => {
           <input name="email" type="email" required/>
           <label>Password</label>
           <input name="password" type="password" required/>
-          <button className="submit w-100" type="submit"><Link className="text-decoration-none"to="../">CONTINUE</Link></button>
+          <Link className="text-decoration-none"to="../"><button className="submit w-100" type="submit">CONTINUE</button></Link>
           <p>Don’t have an account yet?</p>
           <Link to={'../signup'}>Sign-Up</Link>
         </div>
